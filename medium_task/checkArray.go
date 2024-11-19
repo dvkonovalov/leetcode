09bb382c0681c5b8526413d69f0b0c58612b48a6
dvkonovalov <dvkonovalov@avito.ru>
2024-11-19 19:45:25 +0300

    done 2772. Apply Operations to Make All Array Elements Equal to Zero

--- medium_task/checkArray.go
+++ medium_task/checkArray.go
@@ -1,24 +1,23 @@
 package medium_task
 
 func checkArray(nums []int, k int) bool {
-	minus := make([]int, k)
-	for i := 0; i < len(nums)-k+1; i++ {
-		if minus[0] > nums[i] {
+	memory := make([]int, k)
+	minus := 0
+	for i := 0; i < len(nums); i++ {
+		minus -= memory[i%k]
+		if nums[i]-minus < 0 {
 			return false
 		}
-		razn := nums[i] - minus[0]
-		for j := 1; j < k; j++ {
-			minus[j-1] = minus[j] + razn
-		}
-		minus[k-1] = 0
 
-	}
-	pos := 0
-	for i := len(nums) - k + 1; i < len(nums); i++ {
-		if minus[pos] != nums[i] {
-			return false
+		if i < len(nums)-k {
+			memory[i%k] = nums[i] - minus
+			minus = nums[i]
+		} else {
+			if nums[i]-minus != 0 {
+				return false
+			}
 		}
-		pos++
+
 	}
 	return true
 }
